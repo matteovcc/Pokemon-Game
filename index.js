39c8b59813552afb71823479c00747b66c9146bc
matteovcc <matteovcc99@gmail.com>
2023-02-25 23:18:51 +0100

    Caricato il  file tiled

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const canvas = document.querySelector("canvas")
-const ctx = canvas.getContext("2d")
+const ctx = canvas.getContext("2d") // 2d context
+
 
 canvas.width = 1024
 canvas.height = 576
 
-ctx.fillStyle = "red"
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+ctx.fillStyle = "antiquewhite"
+
+ctx.fillRect(0, 0, canvas.width, canvas.height) // clear the canvas
+
+
+
+const image = new Image() // create a new image
+
+image.src = "./images/Pellet Town.png" 
+
+
+image.onload = () => {
+    ctx.drawImage(image, 0, 0) //disegnamo l'immagine al caricare della pagina
+}
 //console.log(canvas)
