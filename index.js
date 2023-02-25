71f8d56fe0854cab762ac1250d75fa7ac4068ab5
matteovcc <matteovcc99@gmail.com>
2023-02-25 23:09:12 +0100

    Project setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+canvas.width = 1024
+canvas.height = 576
+
+ctx.fillStyle = "red"
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+//console.log(canvas)
