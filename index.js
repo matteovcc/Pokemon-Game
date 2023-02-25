3b2aba38e77b8c5a740a6c98d3d4650dc0e2be09
matteovcc <matteovcc99@gmail.com>
2023-02-26 00:45:19 +0100

    creazione del gicoatore (non completa)

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const canvas = document.querySelector("canvas")
-const ctx = canvas.getContext("2d") // 2d context
-
+const c = canvas.getContext("2d") // 2d context
 
 canvas.width = 1024
 canvas.height = 576
 
-ctx.fillStyle = "antiquewhite"
-
-ctx.fillRect(0, 0, canvas.width, canvas.height) // clear the canvas
+c.fillStyle = "white"
 
 
+c.fillRect(0, 0, canvas.width, canvas.height) // clear the canvas
 
-const image = new Image() // create a new image
 
+
+const image = new Image() // creiamo la immagine del background canvas
 image.src = "./images/Pellet Town.png" 
 
+const playerImage = new Image() 
+playerImage.src = "./images/playerDown.png" //creiamo il player
 
 image.onload = () => {
-    ctx.drawImage(image, -700, -500) //disegnamo l'immagine al caricare della pagina -700px asse x e -500px asse y
-
+    c.drawImage(image, -785, -650) //disegnamo l'immagine al caricare della pagina -700px asse x e -500px asse y
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - playerImage.width / 2,
+        canvas.height / 2 - playerImage.height / 2,   //queste rappresentano le coordinate del player 
+        playerImage.width / 4,
+        playerImage.height
+    ) 
 }
-//console.log(canvas)
+
+
+
+
+    
+
+
+
